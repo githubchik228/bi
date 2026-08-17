e5fd6017cce8f6d7342a1b806572d7563f11fe92
githubchik228 <144711603+githubchik228@users.noreply.github.com>
2026-08-17 10:45:34 +0500

    Enforce automatic rollback on expired license

--- src/RustPerformanceSuite/App.xaml.cs
+++ src/RustPerformanceSuite/App.xaml.cs
@@ -1,7 +1,22 @@
 using System.Windows;
+using RustPerformanceSuite.Services;
 
 namespace RustPerformanceSuite;
 
 public partial class App : Application
 {
+    private readonly LicenseService _license = new();
+    private readonly OptimizationEngine _optimizer;
+
+    public App()
+    {
+        _optimizer = new OptimizationEngine(new ChangeTracker());
+        Startup += OnStartup;
+    }
+
+    private async void OnStartup(object sender, StartupEventArgs e)
+    {
+        if (_license.HasExpired())
+            await _optimizer.RestoreAllAsync();
+    }
 }
