ec3ab04e411a6e11ad4e925baa79b77bcc3c94a5
githubchik228 <144711603+githubchik228@users.noreply.github.com>
2026-08-18 16:59:52 +0500

    Add using directive for System.Windows.Controls

--- tools/UndOptiKeygen/MainWindow.xaml.cs
+++ tools/UndOptiKeygen/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
+using System.Windows.Controls;
 namespace UndOptiKeygen;
 public partial class MainWindow : Window
 {
